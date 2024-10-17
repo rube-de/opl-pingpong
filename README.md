452a4be0c43a02fabed63564273444830c817503
beruf <rube@ruf2.de>
2024-10-17 21:29:38 +0200

    feat: new taks and updated README

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# opl-pingpong
-Send messages via Oasis OPL wrapper
+# PingPong via Oasis Privacy Layer (OPL)
+This example example lets you send a message via the Oasis Privacy Layer ([OPL])
+
+[OPL]: https://docs.oasis.io/dapp/opl/
+
+## Setup 
+
+Install the necessary NPM dependencies using `pnpm` or module manager of choice.
+
+```sh
+pnpm install
+```
+
+Then, prepare your hex-encoded private key for paying the deployment gas fee
+and store it as an environment variable:
+
+```sh
+export PRIVATE_KEY=0x...
+```
+
+You will need to obtain TEST tokens on both networks.
+
+## Running the Example
+
+The see the full example in action, run
+
+```sh
+pnpm hardhat full-pingpong
+```
+
+This will do the following:
+
+- Deploy contracts on both testnets (BSC Testent, Sapphire Testnet)
+- Send a message (`"Hello from BSC"`) from the BSC Testnet to the Sapphire Testnet
+- Listen to the messege sent via OPL on the Sapphire Testnet
+
+If you want to run the steps one at a time, read the following chapters.
+
+### Deploying Contracts
+
+Deploy the contracts on the host network (BSC Testnet) and the enclav network
+(Sapphire Testnet):
+
+```sh
+pnpm hardhat deploy-pingpong
+```
+
+### Sending the Ping Message
+
+Sending the ping message from the host network (BSC Testnet):
+
+```sh
+pnpm hardhat send-ping --ping-addr <Ping contract address from above>
+```
+
+### Verifying the Message
+
+Verifying that the ping message arrived on the enclave network (Sapphire-Testnet):
+
+```sh
+pnpm hardhat verify-ping --pong-addr <Pong contract address from above>
+```
+
+## Tasks
+
+Running the example is done via hardhat tasks. You can find them in  `./tasks/index.ts`
--- tasks/index.ts
+++ tasks/index.ts
@@ -1,4 +1,31 @@
 import { task, subtask } from "hardhat/config";
+import { assert } from "console";
+
+task("full-pingpong")
+  .addOptionalParam("message", "The message that should be bridged", "Hello from BSC")
+  .addOptionalParam("hostNetwork", "Network to deploy the Host contract on", "bsc-testnet")
+  .addOptionalParam("hostMainChain", "Main chain of the host network", "bsc")
+  .addOptionalParam("enclaveNetwork", "Network to deploy the Enclave contract on", "sapphire-testnet")
+  .setAction(async ({message, hostNetwork, hostMainChain, enclaveNetwork}, hre) => {
+    const { pingAddr, pongAddr } = await hre.run("deploy-pingpong", {
+      hostNetwork,
+      hostMainChain,
+      enclaveNetwork
+    });
+    console.log("===========================");
+    await hre.run("send-ping", {
+      pingAddr,
+      message,
+      hostNetwork,
+    });
+    console.log("===========================");
+    await hre.run("verify-ping", {
+      pongAddr,
+      message,
+      enclaveNetwork
+    });
+  })
+
 
 task('deploy-pingpong')
   .addOptionalParam("hostNetwork", "Network to deploy the Host contract on", "bsc-testnet")
@@ -23,6 +50,7 @@ task('deploy-pingpong')
         pingAddr,
         hostMainChain
     });
+    return { pingAddr, pongAddr };
   });
 
 subtask("deployPing")
@@ -63,3 +91,64 @@ subtask("nextAddress")
     const nonce = await ethers.provider.getTransactionCount(signerAddr);
     return ethers.getCreateAddress({ from: signerAddr, nonce });
   })
+
+
+task("send-ping")
+  .addParam("pingAddr", "Address of the Ping contract")
+  .addOptionalParam("message", "The message that should be bridged", "Hello from BSC")
+  .addOptionalParam("hostNetwork", "Network to deploy the Host contract on", "bsc-testnet")
+  .setAction(async ({pingAddr, message, hostNetwork}, hre) => {
+    await hre.switchNetwork(hostNetwork);
+    console.log(`Sending message on ${hre.network.name}...`);
+    const ping = await hre.ethers.getContractAt("Ping", pingAddr);
+    const signerBSC = await hre.ethers.provider.getSigner();
+    const value = hre.ethers.parseEther('0.001');
+    const pingWithAttackedSigner = ping.connect(signerBSC);
+
+    const result = await pingWithAttackedSigner.getFunction('startPing')
+      .send(hre.ethers.encodeBytes32String(message), { value });
+    await result.wait();
+    console.log("Message sent");
+  })
+
+  task('verify-ping')
+  .addParam('pongAddr', 'Address of the Pong contract')
+  .addOptionalParam("message", "The message that should be bridged", "Hello from BSC")
+  .addOptionalParam("enclaveNetwork", "Network to deploy the Enclave contract on", "sapphire-testnet")
+  .setAction(async ({pongAddr, message, enclaveNetwork}, hre) => {
+    await hre.switchNetwork(enclaveNetwork);
+    console.log(`Verifying message on ${hre.network.name}...`);
+    let events;
+    const spinner = ['-', '\\', '|', '/'];
+    let current = 0;
+
+    // Spinner animation
+    const interval = setInterval(() => {
+        process.stdout.write(`\rListing for event... ${spinner[current]}`);
+        current = (current + 1) % spinner.length;
+    }, 150);
+
+    const signer = await hre.ethers.provider.getSigner();
+    const pong = await hre.ethers.getContractAt("Pong", pongAddr);
+    const pongWithSigner = pong.connect(signer);
+
+    do {
+      const block = await hre.ethers.provider.getBlockNumber();
+
+      events = await pongWithSigner.queryFilter('MessageReceived', block - 10, 'latest');
+      if (events.length === 0) {
+        await new Promise(resolve => setTimeout(resolve, 60 * 1000));
+      }
+    } while (events.length === 0);
+    
+    // Clear the spinner line
+    clearInterval(interval);
+    process.stdout.write(`\r`); 
+    process.stdout.clearLine(0);
+
+    const parsedEvent = pongWithSigner.interface.parseLog(events[0]);
+    // console.log(parsedEvent);
+    const decoded = hre.ethers.decodeBytes32String(parsedEvent?.args[0]);
+    console.log(`Message received with: ${decoded}`);
+    assert(decoded == message);
+  });
